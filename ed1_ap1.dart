d4dd434b190d7cc83d40344cdc03582d2cc09abd
talecco <othalesborges@gmail.com>
2023-06-22 23:13:07 -0300

    Finalizado

--- ed1_ap1.dart
+++ ed1_ap1.dart
@@ -1,1 +1,29 @@
+void main() {
+  const String nome = "Thales";
+  const String sobrenome = "Borges";
+  int idade = 24;
+  bool ativo = true;
+  double peso = 78.8;
+  String? nacionalidade = null;
 
+  List<dynamic> resultado = [nome, sobrenome, peso];
+  print('Nome completo: ${resultado[0]} ${resultado[1]}');
+
+  if (idade < 18) {
+    print("Idade: $idade (Menor de idade)");
+  } else {
+    print("Idade: $idade (Maior de idade)");
+  }
+  if (ativo == true) {
+    print("Situação:Ativo");
+  } else {
+    print("Situação: Inativo");
+  }
+  print("Peso: $peso");
+
+  if (nacionalidade == null) {
+    print("Nacionalidade: Não informada");
+  } else {
+    print("Nacionalidade: $nacionalidade");
+  }
+}
